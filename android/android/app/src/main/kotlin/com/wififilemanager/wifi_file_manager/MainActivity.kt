e15141d73f23df9b6f9fa59a645309d409a9be11
return <2865248848@qq.com>
2026-05-06 22:34:17 +0800

    修复：Kotlin 编译错误
    - MainActivity.kt：onActivityResult 返回类型修正为 Unit
    - SafFolderPicker.kt：getColumnIndexOrNull 替换为标准 getColumnIndex

--- android/android/app/src/main/kotlin/com/wififilemanager/wifi_file_manager/MainActivity.kt
+++ android/android/app/src/main/kotlin/com/wififilemanager/wifi_file_manager/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.wififilemanager.wifi_file_manager
 
+import android.content.Intent
 import com.ryanheise.audioservice.AudioServiceActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
@@ -9,9 +10,26 @@ class MainActivity : AudioServiceActivity() {
         var channel: MethodChannel? = null
     }
 
+    private var safFolderPicker: SafFolderPicker? = null
+
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
         channel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "com.wififilemanager/media")
+
+        safFolderPicker = SafFolderPicker(this)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "com.wififilemanager/saf")
+            .setMethodCallHandler { call, result ->
+                if (call.method == "pickFolderSaf") {
+                    safFolderPicker?.pickFolderSaf(result)
+                } else {
+                    result.notImplemented()
+                }
+            }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        safFolderPicker?.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
     override fun onDestroy() {
